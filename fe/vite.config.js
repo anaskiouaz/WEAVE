d9aeb62f3de0462893f00291e5c48a8e9498d313
Anas KIOUAZ <155064114+anaskiouaz@users.noreply.github.com>
2026-01-17 17:47:39 +0100

    fix(docker): utilisation du nom de service 'weave-api' dans le proxy Vite pour corriger la connexion backend

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -8,15 +8,15 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'http://weave-api:4000', // <-- REMPLACEZ localhost PAR weave-api
         changeOrigin: true
       },
       '/upload': {
-        target: 'http://localhost:4000',
+        target: 'http://weave-api:4000', // <-- ICI AUSSI
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://localhost:4000',
+        target: 'http://weave-api:4000', // <-- ET ICI
         changeOrigin: true
       }
     }
